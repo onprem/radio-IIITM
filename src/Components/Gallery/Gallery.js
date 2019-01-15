dadae28338520c7e79df6c9c078713f6c62ec8db
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-01-16 01:10:53 +0530

    Gallery component with modified classNames: Carousel Conflict

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -14,55 +14,75 @@ class Gallery extends Component {
     return (
 	   	<div className='contact-container'>
         <div className="content">
-          <div className="radiodiv">
-            <div className="radiologo"><img alt='' src={radio} /></div>
-            <div className="radiotext"><p>Broadcasting Community Radio at <b>ABV IIITM</b> and online</p></div>
+          <div className="vcarousel-container">
+            <div className="vcarousel my-vcarousel vcarousel--translate">
+              <input className="vcarousel__activator" type="radio" name="vcarousel" id="F" checked="checked"/>
+              <input className="vcarousel__activator" type="radio" name="vcarousel" id="G"/>
+              <input className="vcarousel__activator" type="radio" name="vcarousel" id="H"/>
+              <div className="vcarousel__controls">
+                <label className="vcarousel__control vcarousel__control--backward" for="J"></label>
+                <label className="vcarousel__control vcarousel__control--forward" for="G"></label>
+              </div>
+              <div className="vcarousel__controls">
+                <label className="vcarousel__control vcarousel__control--backward" for="F"></label>
+                <label className="vcarousel__control vcarousel__control--forward" for="H"></label>
+              </div>
+              <div className="vcarousel__controls">
+                <label className="vcarousel__control vcarousel__control--backward" for="G"></label>
+                <label className="vcarousel__control vcarousel__control--forward" for="I"></label>
+              </div>
+              <div className="vcarousel__track">
+                <li className="vcarousel__slide">
+                </li>
+                <li className="vcarousel__slide">
+                </li>
+                <li className="vcarousel__slide">
+                </li>
+              </div>
+              <div className="vcarousel__indicators">
+                <label className="vcarousel__indicator" for="F"></label>
+                <label className="vcarousel__indicator" for="G"></label>
+                <label className="vcarousel__indicator" for="H"></label>
+              </div>
+            </div>
           </div>
-          <div className="contact">
-            <h2>CONTACT US</h2>
-            <div className="contact-card">
-              <h4>Studio Phone</h4>
-              <span>Phone: 9889988998</span>
+          <h3>Nukkad Natak</h3>
+          <div className="videos">
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/XgfZntXbSNM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="contact-card">
-              <h4>Station Manager</h4>
-              <span><b>Official Business</b></span>
-              <span>Email: <a href="mailto:radio@iiitm.ac.in">radio@iiitm.ac.in</a></span>
-              
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/jiMhn0p9XAE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>        
+          </div>
+          <h3>Dance</h3>
+          <div className="videos">
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/3iu5nWCS05k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="contact-card">
-              <h4>Assistant Station Manager</h4>
-              <span><b>General Information</b></span>
-              <span>Email: <a href="mailto:asm.radio@iiitm.ac.in">asm.radio@iiitm.ac.in</a></span>
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/nFKyAOXeVH4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="contact-card">
-              <h4>Music Director</h4>
-              <span><b>Promos</b></span>
-              <span>Email: <a href="mailto:md.radio@iiitm.ac.in">md.radio@iiitm.ac.in</a></span>
-              
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/GacZ91zs-is" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="contact-card">
-              <h4>Program Director</h4>
-              <span><b>Radio Scheduling/Automation System</b></span>
-              <span>Email: <a href="mailto:program.radio@iiitm.ac.in">program.radio@iiitm.ac.in</a></span>
-              
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/JSgOkqkzyJc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="contact-card">
-              <h4>Technical Director</h4>
-              <span><b>Equipment and Systems</b></span>
-              <span>Email: <a href="mailto:td.radio@iiitm.ac.in">td.radio@iiitm.ac.in</a></span>
+          </div>
+          <h3>Bundelkhandi Folk Songs</h3>
+          <div className="videos">
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/Gld5KNvKqEo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>        
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/e9cEWbk3WhY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="contact-card">
-              <h4>Mailing Address</h4>
-              <span><b>RADIO-IIITM XX.XFM</b></span>
-              <span>ABV IIITM Campus</span>
-              <span>Morena Link Road</span>
-              <span>Gwalior, 474014</span>
-            </div>
-            <div className="contact-card">
-              <h4>Office Location</h4>
-              <span>ABV IIITM Campus</span>
-              <span>Morena Link Road, Gwalior</span>
+          </div>
+          <h3>Happy Happy!</h3>
+          <div className="videos">
+            <div className="video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/Tgwyj2mG_YU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
           </div>
         </div>
